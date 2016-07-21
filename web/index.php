ad110088c51fa35d4ede24d1e6db4750c1fcbfe9
Marcin <marcin.wolfgraf@gmail.com>
2016-07-21 14:34:04 +0200

    added dependencies

--- web/index.php
+++ web/index.php
@@ -4,6 +4,26 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+#register services
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'host'      => 'loclahost',
+        'dbname'    => 'my_database',
+        'user'      => 'my_username',
+        'password'  => 'my_password',
+        'charset'   => 'utf8mb4',
+    ),
+));
+
+$app['locale'] = 'pl';
+
 $app->get('/', function () {
     return 'helo łorld';
 });
@@ -14,7 +34,5 @@ $app->get('/test', function () {
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Sziema '.$app->escape($name);
 });
-
-
 
 $app->run();
